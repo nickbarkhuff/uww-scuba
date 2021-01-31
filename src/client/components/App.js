827aedec5f744c904f79d0026fa6ab4b6ef02102
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-01-31 09:10:26 -0800

    Add content

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import {Switch, Route} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 import {Section, Spacer} from "./components.js";
+import {pages} from "../pages/pages.js";
+import logo from "../assets/logo.png";
 
 export const App = () => (
-    <div>App</div>
+    <div className="mhFull dFlex fdColumn ff1 bg3 c1">
+        <Section outerClassName="bg1">
+            <header className="c2 pt1 pb1 dFlex aiCenter">
+                <div className="flex1">
+                    <img className="hLogo" src={logo} alt="Logo"/>
+                </div>
+                <nav className="">
+                    Nav
+                </nav>
+            </header>
+        </Section>
+
+        <Switch>
+            {pages.map(page => (
+                <Route key={page.path} path={page.path} exact>
+                    <Helmet><title>{page.name}</title></Helmet>
+                    {page.content}
+                </Route>
+            ))}
+        </Switch>
+
+        <Spacer expand/>
+
+        <Section outerClassName="bg2">
+            <footer className="c1 pt2 pb2 fs2 taCenter fwBold">
+                © {new Date().getFullYear()} Under Water Works
+            </footer>
+        </Section>
+    </div>
 );
--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+import {Section, Spacer} from "../components/components.js";
+import diver from "../assets/diver.jpeg";
 
 export const Home = () => (
-    <div>page</div>
+    <Fragment>
+        <Section innerClassName="mhJumbotron pt2 pb2" bgImage={diver}>
+            <span className="bg2Light dInlineBlock p2 taCenter br1">
+                <h1 className="fs4 pb1 fwBold">Your diving adventure starts here</h1>
+                <p className="fs2">Explore a whole new world, right here in the Pacific Northwest!</p>
+            </span>
+        </Section>
+    </Fragment>
 );
