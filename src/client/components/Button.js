4b1f357e1cc9b800ed715b1b35f381a813629f87
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-02-01 05:42:02 -0800

    Restyle button

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -4,15 +4,14 @@ import {Link} from "react-router-dom";
 export const Button = (props) => {
     const linkClassName = (
         "ButtonChild bg4 c1 p1 br1 fs2 fwBold "
-        + (props.block ? "dBlock" : "dInlineBlock")
+        + (props.block ? "dFlex aiCenter jcCenter hFull" : "dInlineBlock")
     );
+
     return (
-        <div
-            className={
-                "ButtonParent "
-                + (props.block ? "dBlock" : "dInlineBlock")
-            }
-        >
+        <div className={
+            "ButtonParent "
+            + (props.block ? "dBlock hFull" : "dInlineBlock")
+        }>
             {props.raw ? (
                 <a className={linkClassName} href={props.to}>
                     {props.children}
