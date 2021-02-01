4a40ec4c80b08688b5710811ee168712d2448656
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-02-01 04:34:53 -0800

    Add content to non-homepages

--- src/client/pages/Classes.js
+++ src/client/pages/Classes.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 export const Classes = () => (
-    <div>page</div>
+    <Fragment>
+        <div className="SectionSpacing"/>
+        <div className="Gutters">
+            <section className="bg1 p2 br1 bs1">
+                <h2 className="c2 fs4 taCenter">Classes</h2>
+            </section>
+        </div>
+    </Fragment>
 );
--- src/client/pages/Schedule.js
+++ src/client/pages/Schedule.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 export const Schedule = () => (
-    <div>page</div>
+    <Fragment>
+        <div className="SectionSpacing"/>
+        <div className="Gutters">
+            <section className="bg1 p2 br1 bs1">
+                <h2 className="c2 fs4 taCenter">Schedule</h2>
+            </section>
+        </div>
+    </Fragment>
 );
