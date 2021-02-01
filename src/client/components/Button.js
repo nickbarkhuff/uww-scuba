73438c9df3dd6e9371a88cb583bd3e4de4106e93
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-02-01 03:05:39 -0800

    Change Button API

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -2,11 +2,20 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export const Button = (props) => (
-    <div className={props.className || ""}>
-        <div className="ButtonParent">
-            <Link className="ButtonChild dBlock bg4 c1 p1 br1 fs2 fwBold" to={props.to}>
-                {props.children}
-            </Link>
-        </div>
+    <div
+        className={
+            "ButtonParent "
+            + (props.block ? "dBlock" : "dInlineBlock")
+        }
+    >
+        <Link
+            className={
+                "ButtonChild bg4 c1 p1 br1 fs2 fwBold "
+                + (props.block ? "dBlock" : "dInlineBlock")
+            }
+            to={props.to}
+        >
+            {props.children}
+        </Link>
     </div>
 );
