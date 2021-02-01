f2ad57b7b4cdb1cebe685b14fcf63bfc57b0dd50
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-01-31 21:04:56 -0800

    Add YouTubeEmbed component

---
+++ src/client/components/YouTubeEmbed.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const YouTubeEmbed = (props) => (
+    <div className={props.className || ""}>
+        <div className="YouTubeEmbedParent">
+            <iframe
+                className="YouTubeEmbedChild"
+                src={props.src}
+                allowFullScreen
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                frameBorder="0"
+            />
+        </div>
+    </div>
+);
--- src/client/components/components.js
+++ src/client/components/components.js
@@ -1,5 +1,6 @@
 import {App} from "./App.js";
 import {Section} from "./Section.js";
 import {Spacer} from "./Spacer.js";
+import {YouTubeEmbed} from "./YouTubeEmbed.js";
 
-export {App, Section, Spacer};
+export {App, Section, Spacer, YouTubeEmbed};
