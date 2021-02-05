55b6d9c16972d0e8f47dad81d6f4b05925d68bf3
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-02-05 00:41:27 -0800

    Add assets for whats new section

--- src/shared/assets.js
+++ src/shared/assets.js
@@ -9,5 +9,27 @@ import openWater from "./assets/classes/open-water.jpeg";
 import daveIshmael from "./assets/staff/dave-ishmael.png";
 import davidFrye from "./assets/staff/david-frye.png";
 import gretchenSlack from "./assets/staff/gretchen-slack.png";
+import computer from "./assets/whats-new/computer.webp";
+import computerBlog from "./assets/whats-new/computer-blog.webp";
+import pugetSound from "./assets/whats-new/puget-sound.jpg";
+import redondoBeach from "./assets/whats-new/redondo-beach.jpg";
+import rescueDiver from "./assets/whats-new/rescue-diver.jpg";
 
-export {diver, equipment, favicon, hours, logo, discoverScuba, divemaster, openWater, daveIshmael, davidFrye, gretchenSlack};
+export {
+    diver,
+    equipment,
+    favicon,
+    hours,
+    logo,
+    discoverScuba,
+    divemaster,
+    openWater,
+    daveIshmael,
+    davidFrye,
+    gretchenSlack,
+    computer,
+    computerBlog,
+    pugetSound,
+    redondoBeach,
+    rescueDiver
+};
