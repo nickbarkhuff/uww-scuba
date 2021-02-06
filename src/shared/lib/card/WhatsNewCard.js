8d6b37014060a9db7b118c0fcb5f17936c8625f5
Nick Barkhuff <10555701+nickbarkhuff@users.noreply.github.com>
2021-02-06 02:48:01 -0800

    Whats New cards

--- src/shared/lib/card/WhatsNewCard.js
+++ src/shared/lib/card/WhatsNewCard.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+import {database} from "../../../client/database.js";
 import {SquareImage} from "./SquareImage.js";
 
 export const WhatsNewCard = (props) => {
+    const types = database.whatsNewTypes;
+
+    const boxShadow = {
+        [types["blog"]]: "bsWhatsNewBlog",
+        [types["video"]]: "bsWhatsNewVideo",
+        [types["item"]]: "bsWhatsNewItem",
+        [types["class"]]: "bsWhatsNewClass",
+        [types["dive"]]: "bsWhatsNewDive"
+    }[props.type];
+
     return (
-        <div></div>
+        <div className={"ZoomOnHoverChild taCenter br1 p1 hFull " + boxShadow}>
+            <div className={"pb1"}>
+                <SquareImage src={props.image}/>
+            </div>
+            <h3 className={"pb1 fs2 fwBold"}>{props.name}</h3>
+            <p className={"fs1"}>{props.description}</p>
+        </div>
     );
 };
